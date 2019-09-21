aa006f0612096414e0344245b35aba6716a86a3a
Raghav Kaul <raghavkaul1@gmail.com>
2019-09-21 11:15:36 -0700

    Add code for histogram specification
    Refactor and reuse methods from histogram equalization

--- Histogram_Equalization.py
+++ Histogram_Equalization.py
@@ -2,15 +2,22 @@ from PGM import PGMImage
 
 from typing import List
 
-def equalize_histogram(img_name: str, visualize_results: bool):
-    p = PGMImage(img_name)
+
+def transformer_of(p: PGMImage) -> List[int]:
+    """ Calculate a transformer for histogram equalization.
+    
+    The transformer is a list, indexed by intensity values, whose
+    values represent the output intensity required for an equalized
+    histogram, given an input histogram.
+
+    :param p image to equalize the histogram of
+    """
 
     histogram = p.get_histogram(normed=True)
-
     L = p.quantization  # Number of distinct grey levels
 
     # Calculate histogram transformer
-    T_r = [0] * L
+    T_r = [0] * (L + 1)  # len([0, L]) = L + 1
 
     for i in range(len(histogram)):
         if i == 0:
@@ -19,11 +26,17 @@ def equalize_histogram(img_name: str, visualize_results: bool):
             T_r[i] = ((T_r[i - 1] / L) + histogram[i]) * L
 
     # Discretize T_r by taking the ceil
-    T_r = [min(int(t_r + 1), 255) for t_r in T_r]
+    return [min(int(t_r + 1), 255) for t_r in T_r]
 
-    # Transform the image according to T_r
+
+def equalize_histogram(img_name: str, visualize_results: bool):
+    p = PGMImage(img_name)
+
+    T_r = transformer_of(p)
+
     p2 = PGMImage(img_name)
 
+    # Transform the image according to T_r
     for i in range(len(p2.pixels)):
         p2.pixels[i] = b"".join(bytes([T_r[b]]) for b in p2.pixels[i])
 
---
+++ Histogram_Specification.py
@@ -0,0 +1,46 @@
+from PGM import PGMImage
+from Histogram_Equalization import transformer_of
+
+from typing import List
+
+
+def specify_histogram(img_name, specified_img_name, visualize_results=True):
+    p1, p2 = PGMImage(img_name), PGMImage(specified_img_name)
+
+    T_r = transformer_of(p1)
+
+    G_z = transformer_of(p2)
+
+    def inverted_histogram(h: List[int]) -> List[int]:
+        G_i = [0] * len(h)
+        for h_i in h:
+            G_i[h_i] = h_i
+
+        # Populate missing values
+        last_nonzero_intensity = 0
+        for i in range(len(h)):
+            if G_i[i] != 0:
+                last_nonzero_intensity = G_i[i]
+            else:
+                G_i[i] = last_nonzero_intensity
+
+        return G_i
+
+    G_inverse_z = inverted_histogram(G_z)
+
+    p3 = PGMImage(img_name)
+
+    for i in range(len(p3.pixels)):
+        p3.pixels[i] = b"".join(bytes([G_inverse_z[b]]) for b in p3.pixels[i])
+
+    p3.save(f"specified-to-{p2.name}-{p1.name}")
+
+    if visualize_results:
+        p1.show_histogram()
+        p2.show_histogram()
+        p3.show_histogram(title=f"Histogram of {p1.name}, specified to {p2.name}")
+
+
+if __name__ == "__main__":
+    specify_histogram("images/boat.pgm", "images/sf.pgm")
+    specify_histogram("images/f_16.pgm", "images/peppers.pgm")
