c27da76bad9cb55254a52a047fe6d0fdd85ec992
Lee Easson <ljeasson@go.olemiss.edu>
2019-09-23 01:03:23 -0700

    Add processing loop to sampling and quantization

--- Image_Quantization.py
+++ Image_Quantization.py
@@ -1,7 +1,18 @@
 from PGM import PGMImage
+from PIL import Image
 
-def quantize_image():
-    return
+def quantize_image(path, gray_levels):
+    try:  
+        with Image.open(path) as image: 
+            print("Path:",path," Gray Level:",gray_levels)
+            print()
+
+    except IOError: 
+        pass
 
 if __name__ == "__main__":
-    quantize_image()
+    for img in ("images/lenna.pgm", "images/peppers.pgm"):
+        quantize_image(img, 128)
+        quantize_image(img, 32)
+        quantize_image(img, 8)
+        quantize_image(img, 2)
--- Image_Sampling.py
+++ Image_Sampling.py
@@ -1,7 +1,26 @@
 from PGM import PGMImage
+from PIL import Image
+import numpy as np
+import cv2
 
-def sample_image():
-    return
+def sample_image(path, width, height):
+    try:  
+        with Image.open(path) as image: 
+            #orig_width, orig_height = image.size
+
+            print("Path:",path," Width:",width," Height:",height)
+            print()
+
+            image = image.resize((width, height))
+            image.show()
+            
+            #image.save(str(path)+"_new")  
+
+    except IOError: 
+        pass
 
 if __name__ == "__main__":
-    sample_image()
+    for img in ("images/lenna.pgm", "images/peppers.pgm"):
+        sample_image(img, 128, 128)
+        sample_image(img, 64, 64)
+        sample_image(img, 32, 32)
