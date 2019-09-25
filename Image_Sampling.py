97e7f81dcab2b55a89039c7cddcea44feb057d76
Lee Easson <ljeasson@gmail.com>
2019-09-24 17:36:23 -0700

    Change formatting for Image_Sampling.py

--- Image_Sampling.py
+++ Image_Sampling.py
@@ -1,16 +1,17 @@
 from PGM import PGMImage
 from PIL import Image
-import numpy as np
 
 def sample_image(path, sampling_factor):
-    try:  
+    try:
         with Image.open(path) as image: 
             orig_width, orig_height = image.size
 
-            print("Path:",path," Width:",orig_width," Height:",orig_height)
-            print()
+            print("Path:",path,\
+                  "Width:",orig_width,\
+                  "Height:",orig_height)
 
-            image = image.resize((int(orig_width/sampling_factor), int(orig_height/sampling_factor)))
+            image = image.resize((int(orig_width/sampling_factor),\
+                                  int(orig_height/sampling_factor)))
             image = image.resize((orig_width, orig_height))
             image.show()
             
@@ -25,3 +26,4 @@ if __name__ == "__main__":
         sample_image(img, 2)
         sample_image(img, 4)
         sample_image(img, 8)
+
