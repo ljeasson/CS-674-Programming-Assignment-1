e13fec44e1b32612384142a48295d89194919ee0
Raghav Kaul <raghavkaul1@gmail.com>
2019-09-24 20:07:44 -0700

    Image quantization

--- Image_Quantization.py
+++ Image_Quantization.py
@@ -1,18 +1,21 @@
 from PGM import PGMImage
-from PIL import Image
 
-def quantize_image(path, gray_levels):
-    try:  
-        with Image.open(path) as image: 
-            print("Path:",path," Gray Level:",gray_levels)
-            print()
 
-    except IOError: 
-        pass
+def adjust_quantization_and_save(img_name: str, quantization: int):
+    p = PGMImage(img_name)
+    print(f"Adjusting quantization of {img_name} to {quantization}")
+    p.quantization = quantization
+
+    for i in range(p.rows):
+        # Normalize, quantize, and cast to byte string
+        p.pixels[i] = b"".join(
+            [bytes([int((int(px) / 256) * quantization)]) for px in p.pixels[i]]
+        )
+
+    p.save(f"images/quantized-{quantization}-{p.name}")
+
 
 if __name__ == "__main__":
     for img in ("images/lenna.pgm", "images/peppers.pgm"):
-        quantize_image(img, 128)
-        quantize_image(img, 32)
-        quantize_image(img, 8)
-        quantize_image(img, 2)
+        for q in (128, 32, 8, 2):
+            adjust_quantization_and_save(img, q)
